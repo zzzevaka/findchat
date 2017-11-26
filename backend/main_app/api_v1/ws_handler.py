7f37d60fa9de831f55f70a3fec4ba4ac9801e2d9
zzzevaka <zzzevaka@localhost.localdomain>
2017-11-27 00:10:25 +0300

    fix ws

--- backend/main_app/api_v1/ws_handler.py
+++ backend/main_app/api_v1/ws_handler.py
@@ -20,49 +20,26 @@ class WSUpdates(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
 
-    async def single_reader(self):
-        while await self.mpsc.wait_message():
-            sender, message = await self.mpsc.get()
-            self.write_message(message)
-            logging.debug("Got message %s from %s" % (message, sender.name))
-
-    def start_reader(self):
-        return asyncio.ensure_future(self.single_reader())
-
+    @tornado.gen.coroutine
+    def reader(self, channel):
+        logging.debug('start reader')
+        while (yield channel.wait_message()):
+            msg = yield channel.get(encoding='utf-8')
+            self.write_message(msg)
+        logging.debug('stop reader')
+        
     @tornado.gen.coroutine
     def open(self):
-        logging.debug('opening ws connection with %s' % self.request.remote_ip)
-        self.mpsc = Receiver()
-        self.redis_conn = yield self.redis_pool.acquire()
-        self.reader_task = self.start_reader()
-        self.user_id = self.get_cookie('current_user_id', None)
-        if self.user_id:
-            # yield self.redis_conn.subscribe(
-            #     self.mpsc.channel('updates:user:%i' % int(self.user_id))
-            # )
-            yield self.redis_conn.execute_pubsub(
-                'subscribe', 'updates:user:%i' % int(self.user_id)
-            )
         logging.debug('opend ws')
-
-    @tornado.gen.coroutine
-    def on_close(self):
-        yield self.redis_pool.release(self.redis_conn)
-        self.mpsc.stop()
-
-    @tornado.gen.coroutine
-    def on_message(self, msg):
-        logging.debug(json.loads(msg)['type'])
-        msg = json.loads(msg)
-        if msg['type'] == 'subscribe thread':
-            yield self.redis_conn.subscribe(
-                self.mpsc.channel('updates:thread:%i' % msg['thread_id'])
-            )
-            logging.debug('subscribed')
-        if msg['type'] == 'unsubscribe thread':
-            # yield self.redis_conn.unsubscribe(
-            #     'updates:thread:%i' % msg['thread_id']
-            # )
-            yield self.redis_conn.execute_pubsub(
-                'unsubscribe', 'updates:thread:%i' % msg['thread_id']
-            )
+        conn = yield self.redis_pool.acquire()
+        user_id = self.get_cookie('current_user_id', None)
+        if not user_id:
+            return
+        try:
+            ch_name = 'updates:user:%i' % int(user_id)
+            yield conn.execute_pubsub('subscribe', ch_name)
+            channel = conn.pubsub_channels[ch_name]
+            yield self.reader(channel)
+            yield conn.execute_pubsub('unsubscribe', ch_name)
+        finally:
+            self.redis_pool.release(conn)
